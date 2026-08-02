7cde1cd00a0c78c765359375536c4e95b9b2933f
Jonah Francis Benedicto <jonahfrancisbenedicto@outlook.com>
2026-08-02 23:49:35 +1000

    init planet

---
+++ main.py
@@ -0,0 +1,52 @@
+# Newton's Law of Gravitation
+# F = G*m_1*m_2/r**2
+
+# Newton's Second Law
+# F = m*a
+
+# Imports
+import math
+
+class Planet:
+
+    # Constructer Method
+    def init(self, mass, position, velocity, acceleration) -> None:
+        self._mass: float = mass
+        self._position: tuple[float, float, float] = position
+        self._velocity: tuple[float, float, float] = velocity
+        self._acceleration: tuple[float, float, float] = acceleration
+
+    # Methods
+    def get_speed() -> float:
+        return math.sqrt(velocity[0]**2+velocity[1]**2+velocity[2]**2)
+
+    def get_kinetic_energy() -> float:
+        return 0.5 * self._mass * self.get_speed() ** 2
+
+    def get_momentum() -> float:
+        return self._mass * self.get_speed()
+
+    # Getter and Setter Methods
+    def get_mass() -> float:
+        return self._mass
+
+    def set_mass(mass: float) -> None:
+        self._mass = mass
+
+    def get_position() -> tuple[float, float, float]:
+        return self._position
+
+    def set_position(position: tuple[float, float, float]) -> None:
+        self._position = position
+
+    def get_velocity() -> tuple[float, float, float]:
+        return self._velocity
+
+    def set_velocity(velocity: tuple[float, float, float]) -> None:
+        self._velocity = velocity
+
+    def get_acceleration() -> tuple[float, float, float]:
+        return self._acceleration
+
+    def set_acceleration(acceleration: tuple[float, float, float]) -> None:
+        self._acceleration = acceleration

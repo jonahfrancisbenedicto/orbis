98b975ac76410a160b3dbc12bb5a733919e53627
Jonah Francis Benedicto <jonahfrancisbenedicto@outlook.com>
2026-08-03 10:00:37 +1000

    update planet

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-# Newton's Law of Gravitation
-# F_n = G*m_1*m_2/r**2
-
-# Newton's Second Law
-# F_net = m*a
-# a = F_net/m
-
-# Position, Velocity and Acceleration
-
-# Euler Method
-# a = dv/dt
-# v = dx/dt
-# v_new = v_old + a_new * dt
-# x_new = x_old + v_new * dt
-
 # Imports
 import math
 
+
+# Planet Class
 class Planet:
 
+    _STEP_SIZE: float = 1.0
+    _GRAVITATION: float = 6.7
+    _ZERO: tuple[float, float, float] = (0,0,0)
+
     # Constructer Method
-    def init(self, mass, position, velocity, acceleration) -> None:
-        self._mass: float = mass
-        self._position: tuple[float, float, float] = position
-        self._velocity: tuple[float, float, float] = velocity
-        self._acceleration: tuple[float, float, float] = acceleration
+    def init(self, mass, position, velocity, acceleration, neighbours) -> None:
+        self.set_mass(mass)
+        self.set_position(position)
+        self.set_velocity(velocity)
+        self.set_acceleration(_ZERO)
+        self.set_force(_ZERO)
+        self.set_neighbours(neighbours)
 
     # Methods
-    def get_speed() -> float:
-        return math.sqrt(velocity[0]**2+velocity[1]**2+velocity[2]**2)
-
-    def get_kinetic_energy() -> float:
-        return 0.5 * self._mass * self.get_speed() ** 2
-
-    def get_momentum() -> float:
-        return self._mass * self.get_speed()
+    def step() -> None:
+        self.set_force(_ZERO)
+        for neighbour_i in self.get_neighbours():
+            _MAGNITUDE = _GRAVITATION * self.get_mass() * neighbour_i.get_mass()
+            self.set_force(
+                self.get_force()[0] + _MAGNITUDE / 
+                (self.get_position()[0] - neighbour_i.get_position()[0]), 
+                self.get_force()[1] + _MAGNITUDE / 
+                (self.get_position()[1] - neighbour_i.get_position()[1]), 
+                self.get_force()[2] + _MAGNITUDE / 
+                (self.get_position()[2] - neighbour_i.get_position()[2]) 
+            )
+        self.set_acceleration(self.get_force / self.get_mass())
+        self.set_velocity(self.get_velocity() + self.get_acceleration() * _STEP_SIZE)
+        self.set_position(self.get_position() + self.get_velocity() * _STEP_SIZE)
 
     # Getter and Setter Methods
     def get_mass() -> float:
@@ -60,6 +60,17 @@ class Planet:
     def set_acceleration(acceleration: tuple[float, float, float]) -> None:
         self._acceleration = acceleration
 
+    def get_force() -> tuple[float, float, float]:
+        return self._force
+   
+    def set_force(force: tuple[float, float, float]) -> None:
+        self._force = force
+
+    def get_neighbours() -> list[Planet]:
+        return self._neighbours
+
+    def set_neighbours(neighbours: list[Planet]):
+        self._neighbours = neighbours
 
 class System:
     def init(self) -> None:

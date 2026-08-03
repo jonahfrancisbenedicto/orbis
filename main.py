705181829128173620e7c2d1924551ab59b397d7
Jonah Francis Benedicto <jonahfrancisbenedicto@outlook.com>
2026-08-03 10:11:01 +1000

    fix

--- main.py
+++ main.py
@@ -8,53 +8,35 @@ ZERO: tuple[float, float, float] = (0,0,0)
 # Planet Class
 class Planet:
 
-
     # Constructer Method
-    def __init__(self, mass, position, velocity, neighbours) -> None:
+    def __init__(self, 
+         mass: float, 
+         position: tuple[float, float, float], 
+         velocity: tuple[float, float, float]
+    ) -> None:
         self.set_mass(mass)
         self.set_position(position)
         self.set_velocity(velocity)
-        self.set_acceleration(ZERO)
-        self.set_force(ZERO)
-        self.set_neighbours(neighbours)
 
     # Getter and Setter Methods
-    def get_mass() -> float:
+    def get_mass(self) -> float:
         return self._mass
 
-    def set_mass(mass: float) -> None:
+    def set_mass(self, mass: float) -> None:
         self._mass = mass
 
-    def get_position() -> tuple[float, float, float]:
+    def get_position(self) -> tuple[float, float, float]:
         return self._position
 
-    def set_position(position: tuple[float, float, float]) -> None:
+    def set_position(self, position: tuple[float, float, float]) -> None:
         self._position = position
 
-    def get_velocity() -> tuple[float, float, float]:
+    def get_velocity(self) -> tuple[float, float, float]:
         return self._velocity
 
-    def set_velocity(velocity: tuple[float, float, float]) -> None:
+    def set_velocity(self, velocity: tuple[float, float, float]) -> None:
         self._velocity = velocity
 
-    def get_acceleration() -> tuple[float, float, float]:
-        return self._acceleration
-
-    def set_acceleration(acceleration: tuple[float, float, float]) -> None:
-        self._acceleration = acceleration
-
-    def get_force() -> tuple[float, float, float]:
-        return self._force
-   
-    def set_force(force: tuple[float, float, float]) -> None:
-        self._force = force
-
-    def get_neighbours() -> list["Planet"]:
-        return self._neighbours
-
-    def set_neighbours(neighbours: list["Planet"]):
-        self._neighbours = neighbours
-
 class System:
-    def init(self) -> None:
+    def __init__(self) -> None:
         pass
